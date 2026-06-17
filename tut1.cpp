07fbf84b65cd3082b2188004e836c254577a5651
Bharat Gagan <bharatgagan07@gmail.com>
2026-06-17 16:43:13 +0530

    BasicsCodes
    Including Introduction, First Time Code of C++ and Variables and Comments:

---
+++ tut1.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout << "Hello World!" << endl;
+    return 0;
+}
+
+// Variables - They are container whose storing the data.
+
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 10; // Integer variable
+    float b = 3.14; // Float variable
+    char c = 'A'; // Character variable
+    string d = "Hello"; // String variable
+
+    cout << "Integer: " << a << endl;
+    cout << "Float: " << b << endl;
+    cout << "Character: " << c << endl;
+    cout << "String: " << d << endl;
+
+    return 0;
+}
+
+// Comments - They are used to explain the code and make it more readable. They are ignored by the compiler.
+#include<iostream>
+using namespace std;
+int main(){
+    // This is a single line comment
+    cout << "Hello World!" << endl; // This is an inline comment    
+    return 0;
+}
